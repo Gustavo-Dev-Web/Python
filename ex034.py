e5470422a7216c2d49497d0404f418f3b88327a8
Gustavo422 <96140566+Gustavo422@users.noreply.github.com>
2022-02-28 21:35:47 -0300

    Update Ex034

--- ex034.py
+++ ex034.py
@@ -1,9 +1,9 @@
-salario = float(input('Digite seu salário para calcular seu aumento:'))
+salario = float(input('Digite seu salário para calcular seu aumento: R$'))
 
 aumento01 = (15/100*salario) + salario
 aumento02 = (10/100*salario) + salario
 
 if salario <= 1250:
-    print(f'Seu salário passará de {salario} para {aumento01 :.2f}')
+    print(f'Seu salário passará de {salario} R$ para {aumento01 :.2f} R$')
 else:
-    print(f'Seu salário passará de {salario} para {aumento02 :.2f}')    
+    print(f'Seu salário passará de {salario} R$ para {aumento02 :.2f} R$')    
