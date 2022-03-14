bc7ebfcb489b13c63c9170d1ad85ca591c82bfd7
Gustavo422 <96140566+Gustavo422@users.noreply.github.com>
2022-03-14 10:39:50 -0300

    ex063

--- ex063.py
+++ ex063.py
@@ -1,5 +1,11 @@
-acumulador = 0
-a1 = 1
-d = a1
 n = int(input('Digite quantos números da sequência de Fibonacci você quer:'))
-
+t1 = 0
+t2 = 1
+print(t1,t2,end=' ')
+contador = 3
+while contador <= n:
+    t3 = t1 + t2
+    print(t3, end=' ') 
+    t1 = t2
+    t2 = t3
+    contador += 1
