d2e90b8d9f8af4f1b1d16603e8ebf374482247bc
Gustavo422 <96140566+Gustavo422@users.noreply.github.com>
2022-03-16 13:16:29 -0300

    Ex

--- ex069.py
+++ ex069.py
@@ -1,17 +1,20 @@
 M = ContF = cont = 0
-escolha = 's'
-sexo = ''
-while escolha =='s':
+while True:
     idade = int(input('Idade:'))
-    sexo = str(input('Sexo:[M/F] ')).lower().strip()
+    sexo = 't'
+    while sexo not in 'mf':
+        sexo = str(input('Sexo:[M/F] ')).lower().strip()[0]
     if idade > 18:
         cont+=1
-    if sexo in 'f':
-        if idade < 20:
+    if sexo in 'f' and idade < 20:
             ContF+= 1
     if sexo in 'm':
         M += 1  
-    escolha = str(input('Quer continuar?[S/N]:'))  
+    escolha = 't'    
+    while escolha not in 'sn':    
+        escolha = str(input('Quer continuar?[S/N]:'))  
+    if escolha == 'n':
+        break    
 print(f'''Tem {ContF} mulheres com menos de 20 anos
 Ao todo são {M} homens cadastrados
 Tem {cont} pessoas com mais de 18 anos''')    
