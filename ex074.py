7a99c5419d5e4eb760c74f9ba917965c798ee72e
Gustavo422 <96140566+Gustavo422@users.noreply.github.com>
2022-03-18 13:56:08 -0300

    Ex

--- ex074.py
+++ ex074.py
@@ -1,18 +1,7 @@
 from random import randint
-cont = maior = menor = 0
 print(f'Os valores sorteados foram: ',end='')
-while True:
-    cont +=1
-    aleatório = randint(1,10)
-    if cont == 1:
-        maior = menor = aleatório
-    tupla = (f'{aleatório}')
-    print(f'{tupla}',end=' ')
-    if aleatório > maior:
-        maior = aleatório 
-    if aleatório < menor:
-        menor = aleatório    
-    if cont == 5:
-        break
-print(f'\nO menor valor sorteado foi: {menor}')  
-print(f'O maior valor sorteado foi: {maior}')  
+aleatório = (randint(1,10),randint(1,10),randint(1,10),randint(1,10),randint(1,10),randint(1,10),)
+for a in aleatório:
+    print(a,end=' ')
+print(f'\nO menor valor sorteado foi: {min(aleatório)}')  
+print(f'O maior valor sorteado foi: {max(aleatório)}') 
